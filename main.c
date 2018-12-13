ce2611a780ce959282c2130cea4f58414b59b43f
Nikola Terran <nikolaterranthe1@gmail.com>
2018-12-13 11:44:26 -0500

    s

--- main.c
+++ main.c
@@ -10,6 +10,15 @@
 
 #define KEY 1337
 
+union semun {
+	int              val;    /* Value for SETVAL */
+	struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+	unsigned short  *array;  /* Array for GETALL, SETALL */
+	struct seminfo  *__buf;  /* Buffer for IPC_INFO
+   		                     (Linux-specific) */
+};
+
+
 
 int main(){
 
@@ -18,24 +27,50 @@ int main(){
 	printf("please give an argument: (type --help for help)\n");
 	scanf("%s",args);
 	while(1){
+		//for debug only
+		//for our actual program, use argc/argv instead
+
 		if(!strcmp("--help",args)){
 			printf("create: -C\nremove: -r\nview: -v\n");
 			free(args);
 		}else if(!strcmp("-C",args)){
+
+			//create shared memory
 			int shared_mem = shmget(KEY,1024,IPC_CREAT | 0644);
 			char *str = (char*)shmat(shared_mem,0,0);
+			str = malloc(256);
 			printf("write something:\n");
 			scanf("%s",str);
+			free(str);
 			shmdt(shared_mem);
+			
+			
+			
+			//create semaphore
+			printf("oohh\n");
+			int semaphore = semget(KEY,2,IPC_CREAT | 0644);
+			
+		
+			//create file
+			int file = open("test.txt", O_CREAT | O_APPEND | 0644);
+			printf("write to file:\n");
+			scanf("%s",str);
+			write(file, str, sizeof(*str));
+			
+			close(file);
+
+
 			break;
 		}else if(!strcmp("-v",args)){
 			char *str;
 			int seg_id;
 			//printf("id please:\n");
 			//scanf("%d",seg_id);
-			str = (char*)shmat(KEY,(void*) 0x5000000,0);
+			int shared_mem = shmget(KEY,1024,0);
+			str = (char*)shmat(shared_mem,0,0);
 			//printf("wut?\n");
 			printf("%s\n",str);
+			shmdt(shared_mem);
 			break;
 		}else{
 			printf("retry\n");
